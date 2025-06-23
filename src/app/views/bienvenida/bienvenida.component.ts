540262e51e268f1ffc08cac82bd0280ae73a17e8
IamSebx <brunose0806@gmail.com>
2025-06-23 02:09:02 -0500

    Correccion de errores

--- src/app/views/bienvenida/bienvenida.component.ts
+++ src/app/views/bienvenida/bienvenida.component.ts
@@ -3,12 +3,11 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import AOS from 'aos';
-import { FadeInDirective } from '../../animations/fade-in.directive';
 
 @Component({
   selector: 'app-bienvenida',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule, FadeInDirective],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './bienvenida.component.html',
   styleUrls: ['./bienvenida.component.css']
 })
