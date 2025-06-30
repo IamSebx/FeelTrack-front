5228e4d7fbd076d36d9d973fb15bcb3d7bd71d74
IamSebx <brunose0806@gmail.com>
2025-06-30 08:03:11 -0500

    Actualizacion de Despliegue V2

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiUrls: [
-    'https://239c-132-184-55-34.ngrok-free.app'
+    'https://665b-2800-200-f300-18e-8ce6-5c4-820e-3a9d.ngrok-free.app'
   ],
-  defaultApiUrl: 'https://239c-132-184-55-34.ngrok-free.app',
+  defaultApiUrl: 'https://665b-2800-200-f300-18e-8ce6-5c4-820e-3a9d.ngrok-free.app',
   apiTimeout: 30000
 };
